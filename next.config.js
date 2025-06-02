0112205107af684cec5e6edcdc2e0a7465269ea8
null <derek.hz.wen@gmail.com>
2025-06-02 02:56:19 +0000

    vercel deploy

--- next.config.js
+++ next.config.js
@@ -4,21 +4,18 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  output: "export",
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/daily-rhythm" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/daily-rhythm/" : "",
 };
 
-if (process.env.NEXT_PUBLIC_TEMPO) {
-  nextConfig["experimental"] = {
-    // NextJS 13.4.8 up to 14.1.3:
-    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-    // NextJS 14.1.3 to 14.2.11:
-    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
+// Temporarily disabled to fix webpack caching errors
+// if (process.env.NEXT_PUBLIC_TEMPO) {
+//   nextConfig["experimental"] = {
+//     // NextJS 13.4.8 up to 14.1.3:
+//     // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+//     // NextJS 14.1.3 to 14.2.11:
+//     swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
 
-    // NextJS 15+ (Not yet supported, coming soon)
-  };
-}
+//     // NextJS 15+ (Not yet supported, coming soon)
+//   };
+// }
 
 module.exports = nextConfig;
