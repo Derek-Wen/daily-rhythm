007f6e4515377ff7c5254dbeec9b5ef64b185a84
null <derek.hz.wen@gmail.com>
2025-06-02 02:21:10 +0000

    github pages config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    }
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  output: "export",
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/daily-rhythm" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/daily-rhythm/" : "",
 };
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
-    nextConfig["experimental"] = {
-        // NextJS 13.4.8 up to 14.1.3:
-        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-        // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+  nextConfig["experimental"] = {
+    // NextJS 13.4.8 up to 14.1.3:
+    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+    // NextJS 14.1.3 to 14.2.11:
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
 
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
+    // NextJS 15+ (Not yet supported, coming soon)
+  };
 }
 
 module.exports = nextConfig;
